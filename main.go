628c0bf6c5ae85061dd2c3d1035e8aba75d177d9
michaelkot97 <michael.kot97@gmail.com>
2023-05-15 00:41:15 +0300

    Added the expand flag to reduce contants in output by default

--- main.go
+++ main.go
@@ -18,9 +18,11 @@ func cmdUsage() {
 func main() {
 	flag.Usage = cmdUsage
 	var namespace = flag.String("namespace", "default", "Specify the Namespace to run the analyzer on.")
+	var expand = flag.Bool("expand", false, "Use flag to viauslize SCC rule description")
 	flag.Parse()
 
 	context := ""
+	var rules *analyzer.Rules
 
 	client, err := kube.NewClient(context)
 	if err != nil {
@@ -32,21 +34,29 @@ func main() {
 		fmt.Println(err)
 	}
 
-	rules := analyzer.BuildRules()
-
 	rows := [][]string{}
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Namespace", "Service Account", "Rule Description", "SCC"})
-	table.SetAutoMergeCellsByColumnIndex([]int{0, 1})
-	table.SetAutoMergeCells(true)
 	table.SetRowLine(true)
+	table.SetAutoMergeCellsByColumnIndex([]int{0, 1})
+
+	if *expand {
+		rules = analyzer.BuildRules()
+		table.SetHeader([]string{"Namespace", "Service Account", "Rule Description", "SCC"})
+	} else {
+		table.SetHeader([]string{"Namespace", "Service Account", "SCC"})
+	}
 
 	for _, sa := range perms.ServiceAccounts {
 		saSCC := analyzer.CreateServiceAccountMap(perms, sa)
 		for _, scc := range saSCC.SecurityContextConstraints {
-			evaluations := rules.EvaluateSCC(&scc)
-			for _, evaluation := range evaluations {
-				row := []string{*namespace, sa.Name, evaluation, scc.Name}
+			if *expand {
+				evaluations := rules.EvaluateSCC(&scc)
+				for _, evaluation := range evaluations {
+					row := []string{*namespace, sa.Name, evaluation, scc.Name}
+					rows = append(rows, row)
+				}
+			} else {
+				row := []string{*namespace, sa.Name, scc.Name}
 				rows = append(rows, row)
 			}
 		}
